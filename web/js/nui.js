c87fe40c94dbea8b87741ca27e8b849e0aa4bc6a
Josh <57309415+Obtaizen@users.noreply.github.com>
2026-04-07 18:21:22 +0100

    [new progress bar]

--- web/js/nui.js
+++ web/js/nui.js
@@ -23,6 +23,7 @@ window.addEventListener('message', function(event) {
                 radioAudio.play(data.song.file, data.offset || 0, data.volume || 0.7);
                 radioWheel.currentStation = data.stationIndex;
                 radioWheel.setCurrentSong(data.song, data.station);
+                radioWheel.startProgress(data.song.duration, data.offset || 0);
             }
             if (data.spatial) radioAudio.updateSpatial(data.spatial);
             break;
@@ -35,12 +36,14 @@ window.addEventListener('message', function(event) {
             radioWheel.currentStationName = null;
             radioWheel.hideNowPlayingBar();
             radioWheel.updateNowPlaying(null);
+            radioWheel.stopProgress();
             break;
 
         case 'songChanged':
             if (data.song && data.song.file) {
                 radioAudio.changeSong(data.song.file, data.offset || 0);
                 radioWheel.setCurrentSong(data.song);
+                radioWheel.startProgress(data.song.duration, data.offset || 0);
             }
             break;
 
